6176737bf8c71f360e092d87f25a508cb6691527
Brahms <brahmsisme@gmail.com>
2022-05-12 18:40:50 +0200

    update migrationVersion

--- migrations/Version20220512163756.php
+++ migrations/Version20220512163756.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220509175134 extends AbstractMigration
+final class Version20220512163756 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20220509175134 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users CHANGE email email VARCHAR(180) NOT NULL');
+        $this->addSql('ALTER TABLE categories ADD category_order INT NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users CHANGE email email VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE categories DROP category_order');
     }
 }
